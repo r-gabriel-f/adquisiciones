d2887a70fe3227b85c40c157c71470d7109ab974
robert <r.gabriel.f@outlook.com>
2023-09-19 10:02:48 -0400

    se inicio con almacen y cotizacion

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Requerimientos } from "./compononts/Almacen/Requerimientos";
+import { Pedidos } from "./compononts/Cotizacion/Pedidos";
 import { Login } from "./compononts/login/Login";
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/requerimientos" element={<Requerimientos />} />
+        <Route path="/pedidos" element={<Pedidos />} />
+      </Routes>
+    </Router>
   );
 }
 
