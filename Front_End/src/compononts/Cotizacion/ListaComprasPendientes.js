b740e662514f52739187dd66d31da31e1de3fc6d
robert <r.gabriel.f@outlook.com>
2023-10-30 16:53:23 -0400

    se creo el modal eviar cotizacion

--- Front_End/src/compononts/Cotizacion/ListaComprasPendientes.js
+++ Front_End/src/compononts/Cotizacion/ListaComprasPendientes.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { PanelCotizacion } from "../Panel/PanelCotizacion";
 
 export const ListaComprasPendientes = ({ username }) => {
+  const [showLightboxe, setShowLightboxe] = useState(false);
+  const handleOpenLightboxe = () => {
+    
+    setShowLightboxe(true);
+  };
+
+  const handleCloseLightboxe = () => {
+    setShowLightboxe(false);
+  };
   return (
     <div className="flex flex-col">
       <PanelCotizacion />
@@ -88,7 +97,7 @@ export const ListaComprasPendientes = ({ username }) => {
                 <td className="border border-gray-900 py-2 px-4">Pendiente</td>
                 <td className="border border-gray-900 py-2 px-4">2023-09-25</td>
                 <td className="border border-gray-900 py-2 px-4">
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-1 rounded-lg">
+                  <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-1 rounded-lg" onClick={() => handleOpenLightboxe()}>
                     Cotizar
                   </button>
                 </td>
@@ -96,7 +105,99 @@ export const ListaComprasPendientes = ({ username }) => {
             </tbody>
           </table>
         </div>
+        {showLightboxe && (
+          <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-white p-4 rounded shadow-lg w-1/2">
+              <h2 className="text-2xl mb-4">Enviar Cotizacion</h2>
+              <form >
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <label>Item</label>
+                    <input
+                      type="text"
+                      id="item"
+                      name="item"
+                    
+                      className="border border-gray-400 p-2 rounded w-full"
+                      placeholder=""
+                    />
+                  </div>
+                  <div>
+                    <label>Cantidad</label>
+                    <input
+                      type="number"
+                      id="cantidad"
+                      name="cantidad"
+                     
+                      className="border border-gray-400 p-2 rounded w-full"
+                      placeholder="Ingrese la cantidad"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label>Características Técnicas</label>
+                  <input
+                    type="text"
+                    id="caracteristicas"
+                    name="caracteristicas"
+                 
+                    className="border border-gray-400 p-2 rounded w-full"
+                    placeholder="Ingrese detalladamente las características técnicas del item"
+                  />
+                </div>
+                <div>
+                  <label>U - M</label>
+                  <input
+                    type="text"
+                    id="um"
+                    name="um"
+                 
+                    className="border border-gray-400 p-2 rounded w-full"
+                    placeholder="Ingresa la unidad de medida"
+                  />
+                </div>
+                <div>
+                  <label>Orden de Trabajo</label>
+                  <input
+                    type="text"
+                    id="ordenalmacen"
+                    name="ordenalmacen"
+                   
+                    className="border border-gray-400 p-2 rounded w-full"
+                  />
+                </div>
+                <div>
+                  <label>Tiempo de Cumplimiento</label>
+                  <select
+                    id="tiempo"
+                    name="tiempocumplimiento"
+             
+                    className="border border-gray-400 p-2 rounded w-full"
+                  >
+                    <option value="urgente">Urgente</option>
+                    <option value="medio">Medio</option>
+                    <option value="normal">Normal</option>
+                  </select>
+                </div>
 
+                <div className="flex justify-center mt-4">
+                  <button
+                    className="bg-red-500 text-white font-semibold py-2 px-4 rounded hover-bg-red-600 mr-2"
+                    onClick={() => handleCloseLightboxe()}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    type="submit"
+                    className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover-bg-blue-600"
+                  >
+                    Enviar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
         <div className="m-10 font-serif">
           <h3 className="text-2xl">LISTA DE COTIZACIONES</h3>
         </div>
