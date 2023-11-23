667b6f6956376808979951ade4539e31622e00d3
robert <r.gabriel.f@outlook.com>
2023-11-23 17:18:00 -0400

    se añadio la tabla de aprobados

--- Front_End/src/compononts/Cotizacion/CotizacionGerencia.js
+++ Front_End/src/compononts/Cotizacion/CotizacionGerencia.js
@@ -57,6 +57,39 @@ export const CotizacionGerencia = ({ username }) => {
             <tbody></tbody>
           </table>
         </div>
+
+
+
+        <div className="m-10 font-serif">
+          <h3 className="text-2xl">Aprobados</h3>
+        </div>
+        <div className="overflow-x-auto overflow-y-auto h-64">
+          <table className="border-collapse border border-gray-900">
+            <thead className="sticky top-0">
+              <tr className="bg-gray-900 text-white">
+                <th className="border border-gray-900 py-2 px-4">ITEM</th>
+                <th className="border border-gray-900 py-2 px-4">N</th>
+                <th className="border border-gray-900 py-2 px-4">
+                  CARACTERISTICAS
+                </th>
+                <th className="border border-gray-900 py-2 px-4">CANTIDAD</th>
+                <th className="border border-gray-900 py-2 px-4">U-M</th>
+
+                <th className="border border-gray-900 py-2 px-4">
+                  TIEMPO DE CUMPLIMIENTO
+                </th>
+                <th className="border border-gray-900 py-2 px-4">
+                  FECHA DE ACEPTACION
+                </th>
+
+                <th className="border border-gray-900 py-2 px-4">
+                  FECHA DE PEDIDO
+                </th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
