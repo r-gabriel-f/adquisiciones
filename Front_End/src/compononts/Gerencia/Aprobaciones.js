e9dfd8e376b609a62444c2ea4f88ba4e030e0968
robert <r.gabriel.f@outlook.com>
2023-11-27 16:21:23 -0400

    se creo el modal de gerencia aprobaciones

--- Front_End/src/compononts/Gerencia/Aprobaciones.js
+++ Front_End/src/compononts/Gerencia/Aprobaciones.js
@@ -5,7 +5,7 @@ export const Aprobaciones = ({ username }) => {
   const [name, setUsername] = useState("");
   const [aceptacion, setAceptacion] = useState([]);
   const [searchItem, setSearchItem] = useState("");
-  const [searchOrden, setSearchOrden] = useState("");
+  const [showLightboxe, setShowLightboxe] = useState(false);
   useEffect(() => {
     const storedUsername = localStorage.getItem("username");
 
@@ -13,7 +13,14 @@ export const Aprobaciones = ({ username }) => {
       setUsername(storedUsername);
     }
   }, []);
+  const handleOpenLightboxe = () => {
 
+    setShowLightboxe(true);
+  };
+
+  const handleCloseLightboxe = () => {
+    setShowLightboxe(false);
+  };
   const obtenerAceptacion = async () => {
     try {
       const response = await axios.get("http://localhost:3001/aceptacion");
@@ -125,7 +132,7 @@ export const Aprobaciones = ({ username }) => {
                   </td>
 
                   <td className="border border-gray-900 py-2 px-4">
-                    <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-1 rounded-lg">
+                    <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-1 rounded-lg" onClick={() => handleOpenLightboxe()}>
                       Seleccionar Opcion
                     </button>
                     
@@ -135,6 +142,99 @@ export const Aprobaciones = ({ username }) => {
             </tbody>
           </table>
         </div>
+        {showLightboxe && (
+          <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-white p-4 rounded shadow-lg w-1/2">
+              <h2 className="text-2xl mb-4">Editar Requerimiento</h2>
+              <form >
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <label>Item</label>
+                    <input
+                      type="text"
+                      id="item"
+                      name="item"
+                     
+                      className="border border-gray-400 p-2 rounded w-full"
+                      placeholder=""
+                    />
+                  </div>
+                  <div>
+                    <label>Cantidad</label>
+                    <input
+                      type="number"
+                      id="cantidad"
+                      name="cantidad"
+                    
+                      className="border border-gray-400 p-2 rounded w-full"
+                      placeholder="Ingrese la cantidad"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label>Características Técnicas</label>
+                  <input
+                    type="text"
+                    id="caracteristicas"
+                    name="caracteristicas"
+                    
+                    className="border border-gray-400 p-2 rounded w-full"
+                    placeholder="Ingrese detalladamente las características técnicas del item"
+                  />
+                </div>
+                <div>
+                  <label>U - M</label>
+                  <input
+                    type="text"
+                    id="um"
+                    name="um"
+                
+                    className="border border-gray-400 p-2 rounded w-full"
+                    placeholder="Ingresa la unidad de medida"
+                  />
+                </div>
+                <div>
+                  <label>Orden de Trabajo</label>
+                  <input
+                    type="text"
+                    id="ordenalmacen"
+                    name="ordenalmacen"
+                    
+                    className="border border-gray-400 p-2 rounded w-full"
+                  />
+                </div>
+                <div>
+                  <label>Tiempo de Cumplimiento</label>
+                  <select
+                    id="tiempo"
+                    name="tiempocumplimiento"
+                  
+                    className="border border-gray-400 p-2 rounded w-full"
+                  >
+                    <option value="urgente">Urgente</option>
+                    <option value="medio">Medio</option>
+                    <option value="normal">Normal</option>
+                  </select>
+                </div>
+
+                <div className="flex justify-center mt-4">
+                  <button
+                    className="bg-red-500 text-white font-semibold py-2 px-4 rounded hover-bg-red-600 mr-2"
+                    onClick={handleCloseLightboxe}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    type="submit"
+                    className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover-bg-blue-600"
+                  >
+                    Editar Requerimiento
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
